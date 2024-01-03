f4f5f6fde8fbde1699a9bf0c9e78340e75944185
Jack Ryan <jack@canopyteam.org>
2024-01-03 14:41:05 -0600

    using props creating different types of buttons

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,25 @@
 export default {
     template:`
-        <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+        <button 
+        :class="{
+            'border rounded px-5 py-2 disabled:cursor-not-allowed' : true,
+            'bg-blue-200 hover:bg-blue-400': type === 'primary',
+            'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+            'bg-gray-200 hover:bg-gray-400': type === 'muted',
+            'is-loading': processing,
+        }" 
+            :disabled="processing">
             <slot />
         </button>
     `,
-    data() {
-        return {
-            processing: true
+    props:{
+        type: {
+            type: String,
+            default: "primary"
+        },
+        processing: {
+            type: Boolean,
+            default: false
         }
     }
-    
 }
